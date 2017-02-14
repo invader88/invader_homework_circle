6b94778523c61074d2f0135fa3ca7591a96fd132
invader88 <andrey.y21111@gmail.com>
2017-02-14 23:05:59 +0200

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,8 @@
+# invader_homework_circle
+import math
+print("You're going to calculate area of a circle")
+rad = input("Please input radius of a circle in meters : ")
+rad = float(rad)
+c_area = math.pi*rad*rad
+c_area = float(c_area)
+print ("Please see the result below: ", "\n" + "Area of a circle is : ", c_area, "m2")
